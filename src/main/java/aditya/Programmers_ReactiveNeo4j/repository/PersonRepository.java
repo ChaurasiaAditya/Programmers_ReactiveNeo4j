95c256ea8837f5b6fdd977a94ccedd11433e8004
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-03-26 00:15:16 +0530

    added create method in PersonService.java class

--- src/main/java/aditya/Programmers_ReactiveNeo4j/repository/PersonRepository.java
+++ src/main/java/aditya/Programmers_ReactiveNeo4j/repository/PersonRepository.java
@@ -4,4 +4,6 @@ import aditya.Programmers_ReactiveNeo4j.domain.Person;
 import org.neo4j.springframework.data.repository.ReactiveNeo4jRepository;
 
 public interface PersonRepository extends ReactiveNeo4jRepository<Person,Long> {
+
+
 }
---
+++ src/main/java/aditya/Programmers_ReactiveNeo4j/service/PersonService.java
@@ -0,0 +1,41 @@
+package aditya.Programmers_ReactiveNeo4j.service;
+
+import aditya.Programmers_ReactiveNeo4j.domain.Person;
+import aditya.Programmers_ReactiveNeo4j.repository.PersonRepository;
+import org.neo4j.springframework.data.core.ReactiveNeo4jClient;
+import reactor.core.publisher.Mono;
+
+public class PersonService {
+
+
+	private final PersonRepository personRepository;
+	private final ReactiveNeo4jClient reactiveNeo4jClient;
+
+
+	public PersonService(PersonRepository personRepository, ReactiveNeo4jClient reactiveNeo4jClient) {
+		this.personRepository = personRepository;
+		this.reactiveNeo4jClient = reactiveNeo4jClient;
+	}
+
+
+	public Mono<Person> createPerson(String name, int age) {
+		Person person = new Person();
+		person.setName(name);
+		person.setAge(age);
+		this.reactiveNeo4jClient.query("MERGE (p:Person) " +
+				"ON CREATE" +
+				"SET name=$name" +
+				"SET age=$age" +
+				"RETURN p")
+			.bind(person.getName()).to("name")
+			.bind(person.getAge()).to("age").fetch()
+			.one()
+			.map((record) -> {
+				person.setId((Long) record.get("id"));
+				return person;
+			});
+
+		return null;
+
+	}
+}
