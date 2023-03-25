e7e1d1544416b96fe1747bd69b8b7ae7e276125c
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-03-25 23:29:05 +0530

    created PersonRepository and extended to ReactiveNeo$jRepository

---
+++ src/main/java/aditya/Programmers_ReactiveNeo4j/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package aditya.Programmers_ReactiveNeo4j.repository;
+
+import aditya.Programmers_ReactiveNeo4j.domain.Person;
+import org.neo4j.springframework.data.repository.ReactiveNeo4jRepository;
+
+public interface PersonRepository extends ReactiveNeo4jRepository<Person,Long> {
+}
