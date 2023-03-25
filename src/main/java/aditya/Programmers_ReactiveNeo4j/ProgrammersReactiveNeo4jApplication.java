00351577ca9d9ba23120301866d384f0aaa9219e
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-03-25 22:47:01 +0530

    initial commit

---
+++ src/main/java/aditya/Programmers_ReactiveNeo4j/ProgrammersReactiveNeo4jApplication.java
@@ -0,0 +1,13 @@
+package aditya.Programmers_ReactiveNeo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersReactiveNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammersReactiveNeo4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aditya/Programmers_ReactiveNeo4j/ProgrammersReactiveNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package aditya.Programmers_ReactiveNeo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersReactiveNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
