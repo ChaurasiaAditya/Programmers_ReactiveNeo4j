0d11b57f5b9cbb654327ce901ec03853cbc4fa10
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2023-03-25 23:13:30 +0530

    added fields and Required annotations in Person.java

--- src/main/java/aditya/Programmers_ReactiveNeo4j/domain/Person.java
+++ src/main/java/aditya/Programmers_ReactiveNeo4j/domain/Person.java
@@ -1,4 +1,32 @@
 package aditya.Programmers_ReactiveNeo4j.domain;
 
+import lombok.*;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@NodeEntity
 public class Person {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+	private String userName;
+	private int age;
+	@Relationship(type = "LIKES")
+	private List<Language> likes;
+	@Relationship(type = "WORKS")
+	private List<Language> works;
+	@Relationship(type = "HATES")
+	private List<Language> hates;
+
 }
